1b3c988fb16250dacb9703dae3bec03e8a5af37d
Djalma  Bina <djbina2000@yahoo.com.br>
2017-09-16 13:17:35 -0300

    Create index.android.js

---
+++ index.android.js
@@ -0,0 +1,10 @@
+var WebRTC = require('react-native-webrtc');
+var {
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  getUserMedia,
+} = WebRTC;
